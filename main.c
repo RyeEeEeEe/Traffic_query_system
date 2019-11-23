ab4fa30cbc8e38f41d05833da4608d24b22ef432
liukai234 <2679327337@qq.com>
2019-11-23 18:32:36 +0800

    commit

--- main.c
+++ main.c
@@ -3,24 +3,39 @@
  * @Author: 
  * @Date: 2019-11-16 09:33:34
  * @Version: 
- * @LastEditTime: 2019-11-22 22:45:41
+ * @LastEditTime: 2019-11-23 18:32:24
  * @LastEditors: Liu Kai
  */
 #include "myhead.h"
+info *myinfo;
 int main(int args, char *argv[])
 {
     int c = 1;
+    // 源点
     int v0 = 0;
     path p;
+    path mypathDijk;
     dist d;
     Mgraph g;
     char filename[100] = "Text.txt";
+    myinfo = init();
     creat(&g, filename, c);
-    dijkstra(g, v0, p, d);
-    print(g, p, d);
+    // dijkstra(g, v0, p, d);
+    // print(g, p, d, v0);
+    floyd(g, p, d);
+    printf("\n");
+    print(g, p, d, 1);
+    printf("\n");
+    print(g, p, d, v0);
+    linkPrint(myinfo);
     return 0;
 }
 
+/**
+ * @description: 交通网络图的创建
+ * @param {type} 
+ * @return: 
+ */
 void creat(Mgraph *g, char *filename, int c)
 {
     int i, j, k, w;
@@ -31,7 +46,8 @@ void creat(Mgraph *g, char *filename, int c)
         fscanf(pFile, "%d%d", &g->n, &g->e);
         for (i = 0; i < g->n; i++)
         {
-            fscanf(pFile, "%1s", &g->vers[i]);
+            fscanf(pFile, "%s", g->vers[i]);
+            myinfo = insert(pFile, myinfo, g->vers[i]);
         }
         for (i = 0; i < g->n; i++)
         {
@@ -52,7 +68,9 @@ void creat(Mgraph *g, char *filename, int c)
             fscanf(pFile, "%d%d%d", &i, &j, &w);
             g->edges[i][j] = w;
             if (c == 0)
+            {
                 g->edges[j][i] = w;
+            }
         }
         fclose(pFile);
     }
@@ -62,16 +80,21 @@ void creat(Mgraph *g, char *filename, int c)
     }
 }
 
+/**
+ * @description: dijk
+ * @param {type} 
+ * @return: 
+ */
 void dijkstra(Mgraph g, int v0, path p, dist d)
 {
     boolean final[M];
     int i, j, k, v, min, x;
-    printf("start: %c\n", g.vers[v0]);
+    printf("start: %s\n", g.vers[v0]);
     for (v = 0; v < g.n; v++)
     {
         final[v] = FALSE;
         d[0][v] = g.edges[v0][v]; // d中保存的是源点到其他结点的距离
-        if (d[0][v] < FINITY && d[v] != 0)
+        if (d[0][v] < FINITY && d[0][v] != 0)
         {
             p[0][v] = v0; // p[v]用来保存路径
         }
@@ -95,7 +118,7 @@ void dijkstra(Mgraph g, int v0, path p, dist d)
             }
         }
         // 输出本次入选的顶点距离
-        printf("%c---%d\n", g.vers[v], min);
+        printf("%s---%d\n", g.vers[v], min);
         if (min == FINITY)
         {
             return;
@@ -119,28 +142,40 @@ void dijkstra(Mgraph g, int v0, path p, dist d)
     {
         printf("%d ", d[0][i]);
     }
+    printf("\n");
 }
 
-void print(Mgraph g, path p, dist d)
+/**
+ * @description: 源点到其他结点的最短路径,最小运费, v0表示当前源点
+ * @param {type} 
+ * @return: 
+ */
+void print(Mgraph g, path p, dist d, int v0)
 {
     int st[M], i, pre, top = -1;
     for (int i = 0; i < g.n; i++)
     {
-        printf("\nDistancd: %7d, path: ", d[0][i]);
+        printf("\nDistancd: %d, path: ", d[v0][i]);
         st[++top] = i;
-        pre = p[0][i];
+        pre = p[v0][i];
         while (pre != -1)
         {
             st[++top] = pre;
-            pre = p[0][pre];
+            pre = p[v0][pre];
         }
         while (top > 0)
         {
-            printf("%2d", st[top--]);
+            printf("%s ", g.vers[st[top--]]);
         }
     }
+    printf("\n");
 }
 
+/**
+ * @description: 任意一对结点的最短路径,最小费用
+ * @param {type} 
+ * @return: 
+ */
 void floyd(Mgraph g, path p, dist d)
 {
     int i, j, k;
@@ -173,4 +208,57 @@ void floyd(Mgraph g, path p, dist d)
             }
         }
     }
+}
+
+/**
+ * @description: 带头结点链表初始化
+ * @param {type} void
+ * @return: head
+ */
+info *init()
+{
+    info *head;
+    head = (info *)malloc(sizeof(info));
+    head->next = NULL;
+    return head;
+}
+
+/**
+ * @description: 链表尾插
+ * @param {type: node_stu*} head
+ * @return:head
+ */
+info *insert(FILE *pFile, info *head, char *nameCity)
+{
+    info *p, *pre;
+    pre = head;
+    p = head->next;
+    while (p)
+    {
+        pre = p;
+        p = p->next;
+    }
+    p = (info *)malloc(sizeof(info));
+    strcpy(p->nameCity, nameCity);
+    fscanf(pFile, "%d", &p->sumPersion);
+    p->next = pre->next;
+    pre->next = p;
+    return head;
+}
+
+void linkPrint(info *head)
+{
+    info *p, *pre;
+    p = head->next;
+    if (!p)
+    {
+        printf("no City Infomation\n");
+        return;
+    }
+    while (p)
+    {
+        printf("nameCity:%s sumPersion:%d\n", p->nameCity, p->sumPersion);
+        p = p->next;
+    }
+    return;
 }
--- myhead.h
+++ myhead.h
@@ -3,31 +3,47 @@
  * @Author: 
  * @Date: 2019-11-16 10:00:47
  * @Version: 
- * @LastEditTime: 2019-11-22 22:28:34
+ * @LastEditTime: 2019-11-23 18:22:05
  * @LastEditors: Liu Kai
  */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define FINITY 5000
 #define M 20
 #define MAXSTRING 200
 typedef int edgetype;
 typedef char vertextype;
-typedef enum{
+typedef enum
+{
     FALSE,
     TRUE
-}boolean;
+} boolean;
+
+// 城市信息链表
+typedef struct infor
+{
+    char nameCity[MAXSTRING];
+    int sumPersion;
+    struct infor *next;
+} info;
+
 typedef int dist[M][M];
 typedef int path[M][M];
-typedef struct{
-    vertextype vers[MAXSTRING];
-    edgetype edges[M][M];
-    int n,e;
-}Mgraph;
-// 前向声明块
 
+//交通网络图
+typedef struct
+{
+    vertextype vers[M][MAXSTRING];
+    edgetype edges[M][M];
+    int n, e;
+} Mgraph;
+
+// 前向声明块
 void creat(Mgraph *g, char *filename, int c);
 void dijkstra(Mgraph g, int v0, path p, dist d);
-// 源点到其他结点的最短路径,最小运费
-void print(Mgraph g, path p, dist d);
-// 任意一对结点的最短路径,最小费用
-void floyd(Mgraph g,path p, dist d);
+void print(Mgraph g, path p, dist d, int v0);
+void floyd(Mgraph g, path p, dist d);
+info *init();
+info *insert(FILE *pFile, info *head, char *nameCity);
+void linkPrint(info *head);
